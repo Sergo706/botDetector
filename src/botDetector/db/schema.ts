6087d5e0eed2bfbbbceae026c0283bb1b588cc77
Sergio Riavzon <187537278+Sergo706@users.noreply.github.com>
2025-09-05 20:19:50 +0300

    Add user_agent_metadata table and load data query

--- src/botDetector/db/schema.ts
+++ src/botDetector/db/schema.ts
@@ -36,6 +36,36 @@ async function createTables(connection: Connection): Promise<void> {
         );
         `;
 
+const userAgentMetadataSQL = `
+    CREATE TABLE IF NOT EXISTS user_agent_metadata (
+      http_user_agent varchar(255) NOT NULL,
+      metadata_description text,
+      metadata_tool varchar(255) DEFAULT NULL,
+      metadata_category varchar(255) DEFAULT NULL,
+      metadata_link text,
+      metadata_priority varchar(1000) DEFAULT NULL,
+      metadata_fp_risk varchar(50) DEFAULT NULL,
+      metadata_severity varchar(50) DEFAULT NULL,
+      metadata_usage varchar(255) DEFAULT NULL,
+      metadata_flow_from_external varchar(1000) DEFAULT NULL,
+      metadata_flow_from_internal varchar(1000) DEFAULT NULL,
+      metadata_flow_to_internal varchar(1000) DEFAULT NULL,
+      metadata_flow_to_external varchar(1000) DEFAULT NULL,
+      metadata_for_successful_external_login_events varchar(1000) DEFAULT NULL,
+      metadata_comment text,
+      PRIMARY KEY (http_user_agent)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    `;
+    const LOADUSERAGENTDATA = `
+        LOAD DATA LOCAL INFILE './useragent.csv'
+        INTO TABLE user_agent_metadata
+        FIELDS ENCLOSED BY '"' 
+        TERMINATED BY ',' 
+        ESCAPED BY '"' 
+        LINES TERMINATED BY '\r\n'
+        IGNORE 1 LINES;
+    `;
+    
     const createBannedTable = `
         CREATE TABLE IF NOT EXISTS banned (
             canary_id VARCHAR(64) PRIMARY KEY,
@@ -51,6 +81,8 @@ async function createTables(connection: Connection): Promise<void> {
     try {
         await connection.execute(createVisitorsTable);
         await connection.execute(createBannedTable);
+        await connection.execute(userAgentMetadataSQL);
+        await connection.execute(LOADUSERAGENTDATA);
         console.log('Tables created successfully.');
     } catch (error) {
         console.error('Error creating tables:', error);
