29361d11aa3c0d7593bb19a3182076a59aacf690
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2026-03-27 15:22:35 +0300

    lock file

--- src/botDetector/utils/logger.ts
+++ src/botDetector/utils/logger.ts
@@ -1,4 +1,4 @@
-import pino, { Logger } from 'pino';
+import { pino, Logger } from 'pino';
 import { existsSync, mkdirSync } from 'fs';
 import path from 'path';
 import { getConfiguration } from '../config/config.js';
