32a369ba59ab54b0c9e67781aad9dee8cce79c92
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2026-04-22 00:55:09 +0300

    fix: generator cli and load-schema

--- src/botDetector/cli/commands/cleanUp.ts
+++ src/botDetector/cli/commands/cleanUp.ts
@@ -4,22 +4,87 @@ import consola from "consola";
 import { getConfiguration } from "../../config/config.js";
 import { execSync } from "child_process";
 import { __ensureMmdbctl } from "@riavzon/shield-base/internal";
+import { DbConfig, defineConfiguration } from "~~/src/main.js";
 
 export const cleanUp = defineCommand({
     meta: {
         name: 'generate',
         description: 'Run the generator: reads banned and high-risk visitors from your database and compiles them into an optimized binary (based on your generator configuration)'
     },
+  args: {
+        db: { 
+            type: 'enum', 
+            description: 'The data base driver defined in your configuration',
+            required: false,
+            default: "sqlite",
+        },
+        "db-name": { type: 'string', description: 'The database name', required: false },
+        "db-host": { type: 'string', description: 'The database host', required: false },
+        "db-user": { type: 'string', description: 'The database user', required: false },
+        "db-password": { type: 'string', description: 'The database password', required: false },
+        "db-url": { type: 'string', description: 'The database URL (for planetscale)', required: false },
+        mmdbctl: { type: 'string', description: 'The path for the mmdbctl binary', default: 'mmdbctl', required: false },
+        types: { type: 'boolean', description: 'Generate Typescript types', default: false, required: false },
+        delete: { type: 'boolean', description: 'Delete the rows that compiled in this run', default: false, required: false },
+        score: { type: 'string', description: 'The threat score considered a "high threat"', default: '70', required: false },
+    },
 
-    async run() {    
-        const { generator } = getConfiguration();
+    async run({args}) {
+        let dbDriver: DbConfig;
+
+        switch (args.db) {
+                case 'sqlite':
+                    dbDriver = { driver: 'sqlite', name: args["db-name"] };
+                    break;
+                case 'mysql-pool':
+                    dbDriver = {
+                        driver: 'mysql-pool',
+                        host: args["db-host"],
+                        user: args["db-user"],
+                        password: args["db-password"],
+                        database: args["db-name"]
+                    };
+                    break;
+                case 'postgresql':
+                    dbDriver = {
+                        driver: 'postgresql',
+                        host: args["db-host"],
+                        user: args["db-user"],
+                        password: args["db-password"],
+                        database: args["db-name"]
+                    };
+                    break;
+                case 'cloudflare-d1':
+                    dbDriver = { driver: 'cloudflare-d1', bindingName: args["db-name"] };
+                    break;
+                case 'planetscale':
+                    dbDriver = { driver: 'planetscale', url: args["db-url"] };
+                    break;
+                default:
+                    throw new Error(`Unsupported db driver: ${String(args.db)}`);
+            }
+            
+           await defineConfiguration({
+                 store: {
+                    main: dbDriver
+                },
+                generator: {
+                    scoreThreshold: Number(args.score),
+                    generateTypes: args.types,
+                    deleteAfterBuild: args.delete,
+                    mmdbctlPath: args.mmdbctl
+                }
+            });
+
+            const { generator } = getConfiguration();
+
         try {
             execSync(`${generator.mmdbctlPath} --help`, { stdio: 'ignore' });
         } catch {
                 
             consola.warn(`The configurable mmdbctl path cannot be resolved at ${generator.mmdbctlPath}, You will be prompted to install it.`);
             const path = await __ensureMmdbctl();
-            consola.box(`mmdbctl installed successfully. Add the next path in generator.mmdbctlPath configuration option and run the command again: ${path}`);
+            consola.box(`mmdbctl installed successfully. Provide the next path in mmdbctl="<path>" argument option and run the command again: ${path}`);
 
             process.exit(1);
      }
@@ -27,5 +92,6 @@ export const cleanUp = defineCommand({
        consola.start('Starting clean up operation...');
        await runGeneration();
        consola.success('Success!');
+       process.exit(0);
     }
 });
--- src/botDetector/cli/commands/makeTables.ts
+++ src/botDetector/cli/commands/makeTables.ts
@@ -2,16 +2,72 @@ import { defineCommand } from "citty";
 import consola from "consola";
 import { createTables } from "@db/schema.js";
 import { getDb } from "../../config/config.js";
+import { DbConfig } from "../../types/dbTypes.js";
+import { defineConfiguration } from "~~/src/main.js";
 
 export const makeTables = defineCommand({
     meta: {
         name: 'load-schema',
-        description: 'Create database tables'
+        description: 'Create database tables',
     },
+    args: {
+    db: { 
+        type: 'enum', 
+        description: 'The data base driver to use',
+        required: false,
+        default: "sqlite",
+    },
+    "db-name": { type: 'string', description: 'The database name', required: false },
+    "db-host": { type: 'string', description: 'The database host', required: false },
+    "db-user": { type: 'string', description: 'The database user', required: false },
+    "db-password": { type: 'string', description: 'The database password', required: false },
+    "db-url": { type: 'string', description: 'The database URL (for planetscale)', required: false },
+  },
 
-    async run() {
+    async run({args}) {
+       let dbDriver: DbConfig;
+
+       switch (args.db) {
+                case 'sqlite':
+                    dbDriver = { driver: 'sqlite', name: args["db-name"] };
+                    break;
+                case 'mysql-pool':
+                    dbDriver = {
+                        driver: 'mysql-pool',
+                        host: args["db-host"],
+                        user: args["db-user"],
+                        password: args["db-password"],
+                        database: args["db-name"]
+                    };
+                    break;
+                case 'postgresql':
+                    dbDriver = {
+                        driver: 'postgresql',
+                        host: args["db-host"],
+                        user: args["db-user"],
+                        password: args["db-password"],
+                        database: args["db-name"]
+                    };
+                    break;
+                case 'cloudflare-d1':
+                    dbDriver = { driver: 'cloudflare-d1', bindingName: args["db-name"] };
+                    break;
+                case 'planetscale':
+                    dbDriver = { driver: 'planetscale', url: args["db-url"] };
+                    break;
+                default:
+                    throw new Error(`Unsupported db driver: ${String(args.db)}`);
+            }
+            
+           await defineConfiguration({
+                 store: {
+                    main: dbDriver
+                }
+            });
+
         const db = getDb();
         consola.start(`Creating tables for ${db.dialect}...`);
         await createTables(db);
+        process.exit(0);
     }
 });
